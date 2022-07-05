2ed4529c33302fc29c5ecd3319f4ad0c06086299
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2022-07-05 19:07:01 +0700

    update

--- cronJob/index.js
+++ cronJob/index.js
@@ -3,22 +3,34 @@ import { nanoid } from 'nanoid'
 import Promise from 'bluebird'
 
 const updateRoomNameByTime = async (channelID, interval) => {
+    const maxL = "..............".length
+    const lcat = "🧶 ᗢᘏᓗ"
+    const rcat = "ᓚᘏᗢ 🧶"
+    let direction = 1;
+    let curLen = 0;
+
     while(true){
-        let _date = new Date().toLocaleDateString('en-US', { timeZone: 'Asia/Jakarta' })
-        let arrDate = _date.split('/')
-        _date = arrDate[1] + '/' + arrDate[0] + '/' + arrDate[2]
-        let _time = new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Jakarta' })
-        const first = _time.split(' ')[0]
-        const second  = _time.split(' ')[1]
-        let newTime = ""
-        let arr = first.split(':')
+        // let _date = new Date().toLocaleDateString('en-US', { timeZone: 'Asia/Jakarta' })
+        // let arrDate = _date.split('/')
+        // _date = arrDate[1] + '/' + arrDate[0] + '/' + arrDate[2]
+        // let _time = new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Jakarta' })
+        // const first = _time.split(' ')[0]
+        // const second  = _time.split(' ')[1]
+        // let newTime = ""
+        // let arr = first.split(':')
         
-        newTime = arr[0] + ":" + arr[1] + `${arr[2] % 2 == 0 ? ':' : ":"}` + arr[2] + " " + second
-        const time = _date + " " + newTime
-        console.log("huntBot", time)
+        // newTime = arr[0] + ":" + arr[1] + `${arr[2] % 2 == 0 ? ':' : ":"}` + arr[2] + " " + second
+        // const time = _date + " " + newTime
+        // console.log("huntBot", time)
+
+        console.log(curLen)
+        const curCat = direction === 1 ? rcat : lcat
+        let msg = '_'.repeat(curLen) + curCat + '_'.repeat(maxL-curLen)
+        console.log(msg)
+
         try{
             const res = await axios.patch(`https://discord.com/api/v9/channels/${channelID}`, {
-                "name": time,
+                "name": msg,
                 "type": 2,
                 "topic": "",
                 "bitrate": 64000,
@@ -30,7 +42,7 @@ const updateRoomNameByTime = async (channelID, interval) => {
                 "default_reaction_emoji": null
             }, {
                 headers: {
-                    "Authorization": "OTQ0MzA2OTkwMTgyNzA3MjYw.GtRfjX.Z-zvIzFKZu9X1foE6s8jw1tXhnASuJS7c6szrQ",
+                    "Authorization": "OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg",
                     "Content-Type": "application/json"
                 }
             })
@@ -40,55 +52,62 @@ const updateRoomNameByTime = async (channelID, interval) => {
             console.log(e.response.data);
         }
         await Promise.delay(interval)
+        direction = curLen === maxL ? -1 : curLen === 0 ? 1 : direction
+        curLen += direction;
     }
 }
 
-const huntBot = async (msg) => {
+const huntBot = async (msg, token) => {
     
     while(true) {
         let message = msg
         const newID = nanoid(); 
-        let _interval = Math.floor(Math.random() * 7) + 5;
+        let _interval = Math.floor(Math.random() * 1) + 15;
         if(message == 'hunt') {
             let face = Math.floor(Math.random() * 2);
             let type = Math.floor(Math.random() * 4);
-            let amount = Math.floor(Math.random() * 50);
+            let amount = Math.floor(Math.random() * 100);
             
             switch(type) {
                 case 1:
                     message = face == 1 ? `dcf ${amount} h bot` : `dcf ${amount} t bot`
+                    // message = 'dh bot 4'
                     break;
                 case 2:
-                    message = 'dh bot'
+                    message = 'dh bot 1'
                 break;
                 case 3:
-                    message = 'dh bot 2'
+                    message = 'dcf 50 h'
                 break;
                 case 4:
-                    message = 'dlb all bot'
+                    message = 'dh bot 3'
                 break;
                 case 0:
-                    message = 'dinv bot'
+                    message = 'db'
                 break;
+                // case 0:
+                //     message = 'dinv bot'
+                // break;
                 default:
                 // code block
             }
         }
-        
+        else _interval = 14
         console.log(new Date())
         console.log(newID.toString(), message)
         try{
-            const res = await axios.post('https://discord.com/api/v9/channels/934084090397855841/messages', {
-                "content": message,
-                "nonce": newID.toString(),
-                "tts": false
-            }, {
-                headers: {
-                    "Authorization": "OTQ0MzA2OTkwMTgyNzA3MjYw.GtRfjX.Z-zvIzFKZu9X1foE6s8jw1tXhnASuJS7c6szrQ",
-                    "Content-Type": "application/json"
-                }
+            token.forEach(t => {
+                axios.post('https://discord.com/api/v9/channels/934084090397855841/messages', {
+                    "content": message,
+                    "nonce": newID.toString(),
+                    "tts": false
+                }, {
+                    headers: {
+                        "Authorization": t,
+                        "Content-Type": "application/json"
+                    }
+                })
             })
-            // console.log(res.data)
         }
         catch(e) {
             console.log(e.response.data.errors.nonce);
@@ -101,12 +120,12 @@ const findChannelId = async () => {
     try{
         const res = await axios.get('https://discord.com/api/v9/guilds/934084089554821121/channels', {
             headers: {
-                "Authorization": "OTQ0MzA2OTkwMTgyNzA3MjYw.GtRfjX.Z-zvIzFKZu9X1foE6s8jw1tXhnASuJS7c6szrQ",
+                "Authorization": "OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg",
                 "Content-Type": "application/json"
             }
         })
         // console.log(res.data[res.data.length - 1] )
-        const curChannel = res.data.find(c => c.name == 'ngay mai se het buon...ngu' || c.name.includes('AM') || c.name.includes('PM'))
+        const curChannel = res.data.find(c => c.name == 'ngay mai se het buon...ngu' || c.name.includes('AM') || c.name.includes('PM') || c.name.includes('___'))
         // console.log(curChannel)
         return curChannel?.id
     }
@@ -118,9 +137,9 @@ const findChannelId = async () => {
 export default async function startJobs() {
     console.log('Jobs started')
 
-    huntBot('hunt');
+    huntBot('hunt', ["OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg"]);
     await Promise.delay(5000)
-    // huntBot('owo');
+    // huntBot('owo', ["NzU5NDE5NTM0NzY2NTA1OTk1.GbdrcH.jF_ydZZbavRWVRZ02sOuKHMnGzl4ULL6jz6dyI"]);
 
     let cID
     while(!cID) {
@@ -130,6 +149,6 @@ export default async function startJobs() {
         await Promise.delay(5000)
     }
     if(cID)
-        updateRoomNameByTime(cID, 3000)
+        updateRoomNameByTime(cID, 1000)
     else console.log("cID not found");
 }
