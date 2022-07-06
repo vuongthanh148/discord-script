c4ea06000aaee8bca7ffe3ccf6480782920fbf42
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2022-07-06 09:47:25 +0700

    update

--- cronJob/index.js
+++ cronJob/index.js
@@ -57,6 +57,53 @@ const updateRoomNameByTime = async (channelID, interval) => {
     }
 }
 
+const updateNickname = async (interval) => {
+    const maxL = "..............".length
+    const lcat = "🧶 ᗢᘏᓗ"
+    const rcat = "ᓚᘏᗢ 🧶"
+    let direction = 1;
+    let curLen = 0;
+
+    while(true){
+        // let _date = new Date().toLocaleDateString('en-US', { timeZone: 'Asia/Jakarta' })
+        // let arrDate = _date.split('/')
+        // _date = arrDate[1] + '/' + arrDate[0] + '/' + arrDate[2]
+        // let _time = new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Jakarta' })
+        // const first = _time.split(' ')[0]
+        // const second  = _time.split(' ')[1]
+        // let newTime = ""
+        // let arr = first.split(':')
+        
+        // newTime = arr[0] + ":" + arr[1] + `${arr[2] % 2 == 0 ? ':' : ":"}` + arr[2] + " " + second
+        // const time = _date + " " + newTime
+        // console.log("huntBot", time)
+
+        console.log(curLen)
+        const curCat = direction === 1 ? rcat : lcat
+        let msg = '_'.repeat(curLen) + curCat + '_'.repeat(maxL-curLen)
+        console.log(msg)
+
+        try{
+            const res = await axios.patch(`https://discord.com/api/v9/guilds/934084089554821121/members/@me`,
+                {
+                    "nick": msg
+                }, {
+                headers: {
+                    "Authorization": "OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg",
+                    "Content-Type": "application/json"
+                }
+            })
+            // console.log(res.data)
+        }
+        catch(e) {
+            console.log(e);
+        }
+        await Promise.delay(interval)
+        direction = curLen === maxL ? -1 : curLen === 0 ? 1 : direction
+        curLen += direction;
+    }
+}
+
 const huntBot = async (msg, token) => {
     
     while(true) {
@@ -140,6 +187,8 @@ export default async function startJobs() {
     huntBot('hunt', ["OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg"]);
     await Promise.delay(5000)
     huntBot('dpray', ["OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg"]);
+
+    updateNickname(1200)
 
     let cID
     while(!cID) {
