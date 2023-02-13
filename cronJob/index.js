8bc77ef372b1ce0e8ca144530cdc5c5515ec15ee
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2023-02-13 07:58:56 +0700

    update chatgpt function

--- cronJob/index.js
+++ cronJob/index.js
@@ -1,6 +1,15 @@
+import { ChatGPTAPI } from 'chatgpt'
 import axios from 'axios'
 import { nanoid } from 'nanoid'
 import Promise from 'bluebird'
+
+const kjv = "Mzk5OTU4MTI3MzIxMTUzNTQ2.GVSdos.XnB8spNy9YLk9XGfGXD6EFjudnQyJM9PqMT528"
+const sdn = "OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg"
+const kjvId = "399958127321153546"
+const userId = kjvId
+const api = new ChatGPTAPI({
+    apiKey: "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"
+})
 
 const updateRoomNameByTime = async (channelID, interval) => {
     const maxL = "..............".length
@@ -42,7 +51,7 @@ const updateRoomNameByTime = async (channelID, interval) => {
                 "default_reaction_emoji": null
             }, {
                 headers: {
-                    "Authorization": "OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg",
+                    "Authorization": kjv,
                     "Content-Type": "application/json"
                 }
             })
@@ -59,8 +68,10 @@ const updateRoomNameByTime = async (channelID, interval) => {
 
 const updateNickname = async (interval) => {
     const maxL = "..............".length
-    const lcat = "🧶 ᗢᘏᓗ"
-    const rcat = "ᓚᘏᗢ 🧶"
+    // const lcat = "🧶 ᗢᘏᓗ"
+    // const rcat = "ᓚᘏᗢ 🧶"
+    const lcat = "vente"
+    const rcat = "vente"
     let direction = 1;
     let curLen = 0;
 
@@ -89,7 +100,7 @@ const updateNickname = async (interval) => {
                     "nick": msg
                 }, {
                 headers: {
-                    "Authorization": "OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg",
+                    "Authorization": "Mzk5OTU4MTI3MzIxMTUzNTQ2.GrmlDZ.TMRq19ecE5NbN2KXNP3ZxZ3oPSAR4r15s3fk4A",
                     "Content-Type": "application/json"
                 }
             })
@@ -167,38 +178,139 @@ const findChannelId = async () => {
     try{
         const res = await axios.get('https://discord.com/api/v9/guilds/934084089554821121/channels', {
             headers: {
-                "Authorization": "OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg",
+                "Authorization": kjv,
                 "Content-Type": "application/json"
             }
         })
-        // console.log(res.data[res.data.length - 2] )
-        // console.log(res.data[res.data.length - 1] )
-        const curChannel = res.data.find(c => c.permission_overwrites.find(p => p.id == "944306990182707260") )
+        const curChannel = res.data.find(c => c.permission_overwrites.find(p => p.id == "399958127321153546" && p.allow == "1049616") )
         // console.log(curChannel)
         return curChannel?.id
+    }
+    catch(e) {
+        console.log(e);
+        // console.log(e.response.data);
+    }
+}
+
+const updateNameByLyrics = async (channelID, interval) => {
+    let curLyrics = ""
+    checkForLyrics(curLyric)
+    while(true){
+        await sendRequestUpdateRoomName(msg, channelID)
+        await Promise.delay(interval)
+        direction = curLen === maxL ? -1 : curLen === 0 ? 1 : direction
+        curLen += direction;
+    }
+}
+
+const sendRequestUpdateRoomName = async (msg, channelID) => {
+    try{
+        const res = await axios.patch(`https://discord.com/api/v9/channels/${channelID}`, {
+            "name": msg,
+            "type": 2,
+            "topic": "",
+            "bitrate": 64000,
+            // "user_limit": 0,
+            "nsfw": false,
+            "flags": 0,
+            "rate_limit_per_user": 0,
+            "rtc_region": null,
+            "default_reaction_emoji": null
+        }, {
+            headers: {
+                "Authorization": kjv,
+                "Content-Type": "application/json"
+            }
+        })
+        // console.log(res.data)
     }
     catch(e) {
         console.log(e.response.data);
     }
 }
 
+const getChannelMessages = async (channelID, limit) => {
+    try{
+        const res = await axios.get(`https://discord.com/api/v9/channels/${channelID}/messages?limit=${limit}`, {
+            headers: {
+                "Authorization": kjv,
+                "Content-Type": "application/json"
+            }
+        })
+
+        let latestQuestion = ""
+        for(const msg of res.data) {
+            if(msg.author.id == userId && msg.referenced_message?.content.includes("_ask ")) return latestQuestion
+            else if(msg.content.includes('_ask ')) latestQuestion = msg
+
+        }
+        console.log("latestQuestion: ", latestQuestion);
+        return latestQuestion
+        // if(res.data[0].content === "latestRequest") return []
+        // else {
+        //     latestRequest = res.data[0].content
+        //     return res.data
+        // }
+    }
+    catch(e) {
+        console.log(e);
+    }
+}
+
+const sendMessageToChannel = async (channelID, referMessageId, content) => {
+    try{
+        const res = await axios.post(`https://discord.com/api/v9/channels/${channelID}/messages`, {
+            "content": content,
+            "nonce": Date.now(),
+            "tts": false,
+            "message_reference": {
+                "message_id": referMessageId
+            }
+        }, {
+            headers: {
+                "Authorization": kjv,
+                "Content-Type": "application/json"
+            }
+        })
+        // console.log(res.data)
+    }
+    catch(e) {
+        console.log(e.response.data);
+    }
+}
+
+const chatGPT = async (channelID, interval) => {
+    let curQuestion = ''
+    while(true){
+        const latestQuestion = await getChannelMessages(channelID, 5)
+        console.log(new Date().toString(), " - latestQuestion: ", latestQuestion);
+
+        if(latestQuestion && latestQuestion.content.replace("_ask ","") != curQuestion) {
+            curQuestion = latestQuestion.content.replace("_ask ","")
+            //call chatgpt API 
+            const res = await api.sendMessage(curQuestion)
+            console.log(res.text)
+            //send message back to discord
+            await sendMessageToChannel(channelID, latestQuestion.id, res.text)
+            await Promise.delay(interval)
+        }
+    }
+}
+
 export default async function startJobs() {
     console.log('Jobs started')
-
-    // huntBot('hunt', ["OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg"]);
-    // await Promise.delay(5000)
-    // huntBot('dpray', ["OTQ0MzA2OTkwMTgyNzA3MjYw.Gq6OS9.x5T3xx9eHdvt5852ZZgdAhwT6TocEiEAxXyPGg"]);
-
-    // updateNickname(3000)
 
     let cID
     while(!cID) {
         cID = await findChannelId()
-        if(cID) console.log("found cID", cID);
+        if(cID) {
+            console.log("found cID", cID);
+            break
+        }
         else console.log("not found cID");
-        await Promise.delay(30000)
+        // await Promise.delay(30000)
     }
     if(cID)
-        updateRoomNameByTime(cID, 1200)
+        chatGPT(cID, 3000)
     else console.log("cID not found");
 }
